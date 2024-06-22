313793d4bba5b26cf06ef678e6266009445411a2
Tiago <tyago_tka48@hotmail.com>
2024-06-22 18:46:50 +0100

    update project (#2)
    * v0.1.1
    
    * docs: update readme
    
    * test: config tests
    
    * test: fix collect coverage path
    
    * chore: update testing library to 14
    
    * build: update react config render
    
    * test: user table
    
    * test: add user form
    
    * test: update coverage threshold
    
    * test: edit user form
    
    * test: update jest configuration
    
    ---------
    
    Co-authored-by: Tiago Aguiar <tyago_tka48@hotmail.com>

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
-![](https://img.shields.io/badge/React-16.13-61dafb?logo=react)
+![](https://img.shields.io/badge/React-18.3-61dafb?logo=react)
 ![](https://img.shields.io/github/last-commit/tiagoc-aguiar/react-crud-artigo1?color=e05042&logo=git)
-![Lines of code](https://img.shields.io/tokei/lines/github/tiagoc-aguiar/react-crud-artigo1)
+![GitHub commit activity](https://img.shields.io/github/commit-activity/m/TiagoC-Aguiar/react-crud-artigo1)
+![GitHub top language](https://img.shields.io/github/languages/top/TiagoC-Aguiar/react-crud-artigo1)
+
+
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
@@ -8,7 +11,7 @@ This project was bootstrapped with [Create React App](https://github.com/faceboo
 
 In the project directory, you can run:
 
-### `npm start`
+### `yarn start`
 
 Runs the app in the development mode.<br />
 Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
@@ -16,12 +19,12 @@ Open [http://localhost:3000](http://localhost:3000) to view it in the browser.
 The page will reload if you make edits.<br />
 You will also see any lint errors in the console.
 
-### `npm test`
+### `yarn test`
 
 Launches the test runner in the interactive watch mode.<br />
 See the section about [running tests](https://facebook.github.io/create-react-app/docs/running-tests) for more information.
 
-### `npm run build`
+### `yarn build`
 
 Builds the app for production to the `build` folder.<br />
 It correctly bundles React in production mode and optimizes the build for the best performance.
@@ -31,7 +34,7 @@ Your app is ready to be deployed!
 
 See the section about [deployment](https://facebook.github.io/create-react-app/docs/deployment) for more information.
 
-### `npm run eject`
+### `yarn eject`
 
 **Note: this is a one-way operation. Once you `eject`, you can’t go back!**
 
@@ -67,6 +70,6 @@ This section has moved here: https://facebook.github.io/create-react-app/docs/ad
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/deployment
 
-### `npm run build` fails to minify
+### `yarn build` fails to minify
 
 This section has moved here: https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify
---
+++ __tests__/components/tables/UserTable.test.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import UserTable from "../../../src/components/tables/UserTable";
+
+
+describe("UserTable component", () => {
+  const usersData = [
+    { id: 1, name: "John", username: "john123" },
+    { id: 2, name: "Jane", username: "jane456" },
+  ];
+
+  const editRowMock = jest.fn();
+  const deleteUserMock = jest.fn();
+
+  test("renders Edit and Delete buttons for each user", () => {
+    const { getAllByText } = render(
+      <UserTable
+        users={usersData}
+        editRow={editRowMock}
+        deleteUser={deleteUserMock}
+      />
+    );
+
+    const editButtons = getAllByText("Edit");
+    const deleteButtons = getAllByText("Delete");
+
+    expect(editButtons.length).toBe(usersData.length);
+    expect(deleteButtons.length).toBe(usersData.length);
+  });
+
+  test("calls editRow function with correct user data when Edit button is clicked", () => {
+    const { getAllByText } = render(
+      <UserTable
+        users={usersData}
+        editRow={editRowMock}
+        deleteUser={deleteUserMock}
+      />
+    );
+
+    const editButtons = getAllByText("Edit");
+    fireEvent.click(editButtons[0]);
+
+    expect(editRowMock).toHaveBeenCalledWith(usersData[0]);
+  });
+
+  test("calls deleteUser function with correct user id when Delete button is clicked", () => {
+    const { getAllByText } = render(
+      <UserTable
+        users={usersData}
+        editRow={editRowMock}
+        deleteUser={deleteUserMock}
+      />
+    );
+
+    const deleteButtons = getAllByText("Delete");
+    fireEvent.click(deleteButtons[1]);
+
+    expect(deleteUserMock).toHaveBeenCalledWith(usersData[1].id);
+  });
+
+  test("renders a message when there are no users", () => {
+    const { getByText } = render(
+      <UserTable users={[]} editRow={editRowMock} deleteUser={deleteUserMock} />
+    );
+
+    expect(getByText("No users")).toBeInTheDocument();
+  });
+
+  test("does not render Edit and Delete buttons when there are no users", () => {
+    const { queryAllByText } = render(
+      <UserTable users={[]} editRow={editRowMock} deleteUser={deleteUserMock} />
+    );
+
+    const editButtons = queryAllByText("Edit");
+    const deleteButtons = queryAllByText("Delete");
+
+    expect(editButtons.length).toBe(0);
+    expect(deleteButtons.length).toBe(0);
+  });
+});
