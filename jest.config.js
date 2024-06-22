e1e03b3553f90166c5128aede76124aab22f4c42
Tiago Aguiar <taguiar@impresa.pt>
2024-06-22 18:39:04 +0100

    test: update jest configuration

--- jest.config.js
+++ jest.config.js
@@ -10,10 +10,10 @@ module.exports = {
   },
   coverageThreshold: {
     global: {
-      statements: 39,
-      branches: 73,
-      functions: 60,
-      lines: 39,
+      statements: 55,
+      branches: 84,
+      functions: 76,
+      lines: 55,
     },
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.js"],
